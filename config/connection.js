873041f9dca0c4efb831300be0364e1e981f44aa
Brandon Jimenez <bjimenezp09@gmail.com>
2023-01-29 22:07:25 -0600

    added node package

--- config/connection.js
+++ config/connection.js
@@ -3,9 +3,7 @@ require("dotenv").config();
 
 const db = mysql.createConnection({
       host: "localhost",
-      // MySQL username,
       user: process.env.DB_USER,
-      // MySQL password
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
     },
